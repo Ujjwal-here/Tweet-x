8a31b53530db34f7ef724bea485edcc48bc50084
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-17 12:53:52 +0530

    ADD: Add Signup Image

---
+++ src/screens/Signup.jsx
@@ -0,0 +1,22 @@
+import Signup from "../../public/Images/signup_tweetx.svg"
+
+export const Signup = () => {
+    return (
+        <>
+            <h3>TweetX</h3>
+            <button>Login</button>
+            <h1>Create Account</h1>
+            <div>
+                <div>
+                    <input placeholder="Name"/>
+                    <input placeholder="Email"/>
+                    <input placeholder="Password"/>
+                    <input placeholder="Confirm Password"/>
+                </div>
+                <img src={Signup} alt="Signup-Tweetx"/>
+            </div>
+
+            <button>Submit</button>
+        </>
+    )
+}
