4be3d67f4634f66303a0573be73b26edbab3c7a5
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-17 15:31:15 +0530

    --Beautify

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,21 +1,22 @@
-
 export const Signup = () => {
     return (
-        <>
-            <h3>TweetX</h3>
-            <button>Login</button>
-            <h1>Create Account</h1>
-            <div>
-                <div>
-                    <input placeholder="Name"/>
-                    <input placeholder="Email"/>
-                    <input placeholder="Password"/>
-                    <input placeholder="Confirm Password"/>
+        <div className="py-10 px-28">
+            <h3 className="text-[#FF748D] font-medium lg:text-2xl">TweetX</h3>
+            <button className="border-2 border-[#C1C1C1] rounded-xl lg:my-10 lg:py-2 lg:px-16 lg:text-sm lg:font-medium">Login</button>
+            <h1 className="text-[#5D676E] font-semibold lg:mt-8 lg:mb-4 lg:text-3xl">Create Account</h1>
+            <div className="flex flex-row">
+                <div className="flex-1 flex flex-col gap-8 lg:my-8 lg:pr-80">
+                    <input className="bg-[#f9f9f9] rounded lg:p-4 " placeholder="Name"/>
+                    <input className="bg-[#f9f9f9] rounded lg:p-4" placeholder="Email"/>
+                    <input className="bg-[#f9f9f9] rounded lg:p-4" placeholder="Password"/>
+                    <input className="bg-[#f9f9f9] rounded lg:p-4" placeholder="Confirm Password"/>
                 </div>
-                <img src="/Images/signup_tweetx.svg" alt="Signup-Tweetx"/>
+                <div>
+                    <img className="h-96" src="/Images/signup_tweetx.svg" alt="Signup-Tweetx"/>
+                </div>
             </div>
 
-            <button>Submit</button>
-        </>
+            <button className="bg-[#FF748D] text-white rounded font-normal lg:text-sm lg:px-4 lg:py-2">Sign up</button>
+        </div>
     )
 }
