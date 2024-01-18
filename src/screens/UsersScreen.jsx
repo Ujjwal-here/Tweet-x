625b55a9a231ec5569de3cf93498408fb144e6ce
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-18 12:04:33 +0530

    ADD: Add Users Screen

---
+++ src/screens/UsersScreen.jsx
@@ -0,0 +1,11 @@
+import {UserCard} from "../components/UserCard";
+
+export const UsersScreen = () => {
+    return (
+        <div className="lg:mx-96 lg:my-20">
+            <UserCard/>
+            <UserCard/>
+            <UserCard/>
+        </div>
+    )
+}
