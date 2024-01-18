fa46ad94c0501bee0da5b366d2532090922d86b1
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-18 12:05:30 +0530

    CHANGE: Change Nav-Items(li) -> Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import {Link} from "react-router-dom";
+
 export const Navbar = () => {
     return (
         <div className="shadow-md flex flex-row justify-between lg:py-6 lg:px-52">
             <h1 className="text-xl text-[#FF748D] font-semibold">TweetX</h1>
             <ul className="flex flex-row lg:gap-10">
-                <li className="text-[#E8EAED]">Feed</li>
-                <li className="text-[#E8EAED]">Users</li>
-                <li className="text-[#E8EAED]">Profile</li>
+                <Link to="/feed" className="text-[#E8EAED]">Feed</Link>
+                <Link to="/users" className="text-[#E8EAED]">Users</Link>
+                <Link to="/profile" className="text-[#E8EAED]">Profile</Link>
             </ul>
         </div>
     )
