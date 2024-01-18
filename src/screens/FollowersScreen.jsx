837aa6e2e2dd1b7c84f36c34ed4c9aaf6b3176d6
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-18 15:47:07 +0530

    ADD: Add FollowersScreen for user's followers list

---
+++ src/screens/FollowersScreen.jsx
@@ -0,0 +1,11 @@
+import {UserCard} from "../components/UserCard";
+
+export const FollowersScreen = () => {
+    return (
+        <div className="lg:my-20">
+            <UserCard/>
+            <UserCard/>
+            <UserCard/>
+        </div>
+    )
+}
