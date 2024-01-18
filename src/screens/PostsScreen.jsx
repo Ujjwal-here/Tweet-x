7266ce42c00f26e5cbfa3b877ebf866952515955
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-18 15:46:18 +0530

    ADD: Add PostsScreen for user's posts

---
+++ src/screens/PostsScreen.jsx
@@ -0,0 +1,11 @@
+import {PostCard} from "../components/PostCard";
+
+export const PostsScreen = () => {
+    return (
+        <div className="lg:my-16">
+            <PostCard/>
+            <PostCard/>
+            <PostCard/>
+        </div>
+    )
+}
