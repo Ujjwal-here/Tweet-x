e63765df1d745f2cee66d5692fdabc1af0b7bdc7
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-18 15:46:43 +0530

    ADD: Add FollowingScreen for user's following

---
+++ src/screens/FollowingScreen.jsx
@@ -0,0 +1,11 @@
+import {UserCard} from "../components/UserCard";
+
+export const FollowingScreen = () => {
+    return (
+        <div className="lg:my-20">
+            <UserCard/>
+            <UserCard/>
+            <UserCard/>
+        </div>
+    )
+}
