ea3d5845aa8e3d87a22693dd3c7e14f751d52e60
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-20 22:15:39 +0530

    CHANGE: Change div tag to form tag

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,8 @@
 import {Link} from "react-router-dom";
-import useLogin from "../hooks/useLogin";
 
 export const LoginScreen = () => {
     return (
-        <div className="lgpy-10 px-28">
+        <div className="lg:py-10 lg:px-28">
             <h3 className="text-[#FF748D] font-medium lg:text-2xl">TweetX</h3>
             <Link to="/signup">
                 <button
@@ -14,7 +13,7 @@ export const LoginScreen = () => {
 
             <h1 className="text-[#5D676E] font-semibold lg:mt-8 lg:mb-4 lg:text-3xl">Login</h1>
             <div className="flex flex-row">
-            <div className="flex-1 flex flex-col gap-8 lg:my-8 lg:pr-80">
+                <form className="flex-1 flex flex-col lg:gap-8 lg:my-8 lg:pr-80">
                     <input className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Email"/>
                     <input className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Password"/>
                     <div className="flex flex-row justify-between items-center lg:my-5">
@@ -22,10 +21,9 @@ export const LoginScreen = () => {
                         <button className="bg-[#FF748D] text-white rounded font-normal lg:text-sm lg:px-4 lg:py-2">Login
                         </button>
                     </div>
-
-                </div>
+                </form>
                 <div>
-                    <img className="h-96" src="/Images/login-tweetx.svg" alt="SignupScreen-Tweetx"/>
+                    <img className="lg:h-96" src="/Images/login-tweetx.svg" alt="SignupScreen-Tweetx"/>
                 </div>
             </div>
         </div>
