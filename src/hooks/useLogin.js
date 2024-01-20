cb812e29740b2e98c8bfddac95ac58a82839b210
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-20 22:39:37 +0530

    CREATE: Create useLogin hook with all the login functionalities

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,29 @@
+import {useRef, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {signInWithEmailAndPassword} from "firebase/auth"
+import {auth} from "../firebase/config";
+
+const useLogin = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState("")
+    const formRef = useRef()
+    const navigate = useNavigate()
+
+    async function loginHandleSubmit(e) {
+        setIsLoading(true)
+        e.preventDefault()
+        const email = formRef.current[0].value
+        const password = formRef.current[1].value
+        try{
+            const userCredential = await signInWithEmailAndPassword(auth,email,password)
+            localStorage.setItem("token", userCredential.user.accessToken)
+            setIsLoading(false)
+            navigate("/")
+        }
+        catch (e){
+            setError(e.message)
+        }
+    }
+    return {isLoading,setIsLoading,error,setError,formRef,loginHandleSubmit}
+}
+export default useLogin
