0b0d0a1297037825dd75891a6e8835b9079ff8ad
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-20 22:40:31 +0530

    IMPORT: Import all the functionalities from useLogin hook

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 import useLogin from "../hooks/useLogin";
 
 export const LoginScreen = () => {
-    const {formRef} = useLogin()
+    const {isLoading,setIsLoading,error,setError,formRef,loginHandleSubmit} = useLogin()
     return (
         <div className="lg:py-10 lg:px-28">
             <h3 className="text-[#FF748D] font-medium lg:text-2xl">TweetX</h3>
@@ -16,11 +16,11 @@ export const LoginScreen = () => {
             <h1 className="text-[#5D676E] font-semibold lg:mt-8 lg:mb-4 lg:text-3xl">Login</h1>
             <div className="flex flex-row">
                 <form ref={formRef} className="flex-1 flex flex-col lg:gap-8 lg:my-8 lg:pr-80">
-                    <input className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Email"/>
-                    <input className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Password"/>
+                    <input type="email" className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Email" name="email"/>
+                    <input type="password" className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Password" name="password"/>
                     <div className="flex flex-row justify-between items-center lg:my-5">
                         <span className="text-sm">Forgot Password?</span>
-                        <button className="bg-[#FF748D] text-white rounded font-normal lg:text-sm lg:px-4 lg:py-2">Login
+                        <button onClick={loginHandleSubmit} className="bg-[#FF748D] text-white rounded font-normal lg:text-sm lg:px-4 lg:py-2">Login
                         </button>
                     </div>
                 </form>
