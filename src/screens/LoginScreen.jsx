e6993d54ecbe507510204c4248a94c17073df22b
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-20 22:13:35 +0530

    ADD: Add Link to Signup Screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,13 +1,20 @@
+import {Link} from "react-router-dom";
+import useLogin from "../hooks/useLogin";
+
 export const LoginScreen = () => {
     return (
-        <div className="py-10 px-28">
+        <div className="lgpy-10 px-28">
             <h3 className="text-[#FF748D] font-medium lg:text-2xl">TweetX</h3>
-            <button
-                className="border-2 border-[#C1C1C1] rounded-xl lg:my-10 lg:py-3 lg:px-10 lg:text-sm lg:font-medium">Create Account
-            </button>
+            <Link to="/signup">
+                <button
+                    className="border-2 border-[#C1C1C1] rounded-xl lg:my-10 lg:py-3 lg:px-10 lg:text-sm lg:font-medium">Create
+                    Account
+                </button>
+            </Link>
+
             <h1 className="text-[#5D676E] font-semibold lg:mt-8 lg:mb-4 lg:text-3xl">Login</h1>
             <div className="flex flex-row">
-                <div className="flex-1 flex flex-col gap-8 lg:my-8 lg:pr-80">
+            <div className="flex-1 flex flex-col gap-8 lg:my-8 lg:pr-80">
                     <input className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Email"/>
                     <input className="bg-[#f9f9f9] rounded lg:p-4 lg:text-sm" placeholder="Password"/>
                     <div className="flex flex-row justify-between items-center lg:my-5">
