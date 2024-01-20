318893bfc1c41a8bf3224c16ec945393308b7458
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-20 18:38:23 +0530

    ADD: Add Routes for all Screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
-import {Signup} from "./screens/Signup";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HomeScreen} from "./screens/HomeScreen";
+import {UsersScreen} from "./screens/UsersScreen";
+import {FeedScreen} from "./screens/FeedScreen"
+import {ProfileScreen} from "./screens/ProfileScreen";
+import {FollowersScreen} from "./screens/FollowersScreen";
+import {FollowingScreen} from "./screens/FollowingScreen";
+import {PostsScreen} from "./screens/PostsScreen";
+import {LoginScreen} from "./screens/LoginScreen";
+import {SignupScreen} from "./screens/SignupScreen";
 
 function App() {
   return (
-    <div>
-      <Signup/>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path='/login' element={<LoginScreen/>}/>
+              <Route path='/signup' element={<SignupScreen/>}/>
+              <Route element={<HomeScreen/>}>
+                  <Route path="/users" element={<UsersScreen/>}/>
+                  <Route path="/feed" element={<FeedScreen/>}/>
+                  <Route path="/profile" element={<ProfileScreen/>}>
+                      <Route path="posts" element={<PostsScreen/>}/>
+                      <Route path="followers" element={<FollowersScreen/>}/>
+                      <Route path="following" element={<FollowingScreen/>}/>
+                  </Route>
+              </Route>
+
+          </Routes>
+      </BrowserRouter>
   );
 }
 
