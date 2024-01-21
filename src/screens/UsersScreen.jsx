4e8f03a1a20f7588416c93158b5b881588af4aff
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-22 00:32:36 +0530

    ADD: Add logic to fetch all users

--- src/screens/UsersScreen.jsx
+++ src/screens/UsersScreen.jsx
@@ -1,11 +1,35 @@
 import {UserCard} from "../components/UserCard";
+import {useEffect, useState} from "react";
+import {collection, getDocs} from "firebase/firestore";
+import {db} from "../firebase/config";
 
 export const UsersScreen = () => {
+    const [usersData, setUsersData] = useState([])
+
+    async function getAllUsers() {
+        const data = []
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+            data.push(doc.data())
+        })
+        setUsersData(data)
+    }
+
+    useEffect(() => {
+        try {
+            getAllUsers()
+        } catch (e) {
+            console.log(e.message)
+        }
+
+    }, [])
+
+    console.log(usersData)
     return (
         <div className="lg:mx-96 lg:my-20">
-            <UserCard/>
-            <UserCard/>
-            <UserCard/>
+            {usersData && usersData.map((user)=>{
+                return <UserCard key={user.uid} user={user}/>
+            })}
         </div>
     )
 }
