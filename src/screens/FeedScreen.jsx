c4e697cb2c65e12c489e0dcef7f297896a7a15ad
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-21 13:42:44 +0530

    USE: Use context for updating modal state

--- src/screens/FeedScreen.jsx
+++ src/screens/FeedScreen.jsx
@@ -1,13 +1,24 @@
 import {PostCard} from "../components/PostCard";
+import {CreatePostModal} from "../components/CreatePostModal";
+import {useContext, useState} from "react";
+import {TweetXContext} from "../context/TweetXContext";
 
 export const FeedScreen = () => {
+    const {isOpen,setIsOpen} = useContext(TweetXContext)
+
+    function openModalHandler(){
+        setIsOpen(prevState=>!prevState)
+    }
+
     return (
         <div className="lg:mx-96">
-            <button className="bg-[#FF748D] shadow-xl text-white rounded font-normal lg:text-sm lg:px-6 lg:py-2 lg:mx-24 lg:mt-10">Write
+            <button onClick={openModalHandler} data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
+                className="bg-[#FF748D] shadow-xl text-white rounded font-normal lg:text-sm lg:px-6 lg:py-2 lg:mx-24 lg:mt-10">Write
             </button>
             <PostCard/>
             <PostCard/>
             <PostCard/>
+            <CreatePostModal/>
         </div>
     )
 }
