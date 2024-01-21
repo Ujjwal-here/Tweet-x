4af3e6e1f5c968cb35a9dad1518d68253d8f84a4
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-21 16:25:42 +0530

    ADD: Add logic to store post in firebase

--- src/components/CreatePostModal.jsx
+++ src/components/CreatePostModal.jsx
@@ -2,7 +2,7 @@ import {useContext} from "react";
 import {TweetXContext} from "../context/TweetXContext";
 
 export const CreatePostModal = () => {
-    const {isOpen,setIsOpen} = useContext(TweetXContext)
+    const {isOpen, setIsOpen, formRef, createPostHandler} = useContext(TweetXContext)
     function closeModalHandler() {
         setIsOpen(prevState => !prevState)
     }
@@ -29,7 +29,7 @@ export const CreatePostModal = () => {
                                 <span className="sr-only">Close modal</span>
                             </button>
                         </div>
-                        <form className="p-4 md:p-5">
+                        <form ref={formRef} className="p-4 md:p-5">
                             <div className="grid gap-4 mb-4 grid-cols-2">
                                 <div className="col-span-2">
                                     <label htmlFor="name"
@@ -47,7 +47,7 @@ export const CreatePostModal = () => {
                                               placeholder="Write Post description here"></textarea>
                                 </div>
                             </div>
-                            <button type="submit"
+                            <button onClick={createPostHandler} type="submit"
                                     className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                      xmlns="http://www.w3.org/2000/svg">
