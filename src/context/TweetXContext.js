aea30b13c9728dc32e8470cd39d9656f0c74b2f5
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-21 16:25:30 +0530

    ADD: Add logic to store post in firebase

--- src/context/TweetXContext.js
+++ src/context/TweetXContext.js
@@ -1,10 +1,37 @@
-import {createContext, useState} from "react";
+import {createContext, useRef, useState} from "react";
+import {addDoc, collection} from "firebase/firestore";
+import {db} from "../firebase/config";
 
 export const TweetXContext = createContext(null);
 export const TweetXProvider = ({children}) => {
     const [isOpen, setIsOpen] = useState(false)
+    const [isLoading,setIsLoading]=useState(false)
+    const [error,setError]=useState("")
+    const formRef = useRef()
+
+    async function createPostHandler(e){
+        setIsLoading(prevState => !prevState)
+        e.preventDefault()
+        const name= formRef.current[0].value
+        const description= formRef.current[1].value
+        try{
+            const uid= localStorage.getItem("uid")
+            console.log(name,description,uid)
+            await addDoc(collection(db, "posts"), {
+                uid,
+                name,
+                description,
+            });
+            setIsLoading(prevState => !prevState)
+        }
+        catch (e) {
+            setError(e.message)
+            setIsLoading(prevState => !prevState)
+        }
+        setIsOpen(prevState => !prevState)
+    }
     const data={
-        isOpen,setIsOpen
+        isOpen, setIsOpen, formRef,createPostHandler
     }
     return (
         <TweetXContext.Provider value={data}>
