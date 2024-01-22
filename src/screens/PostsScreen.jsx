e9932ea78fce44d3903e6683982074197754275a
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-22 12:43:09 +0530

    ADD: Add logic to fetch all posts of logged in user

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,11 +1,37 @@
 import {PostCard} from "../components/PostCard";
+import {collection, getDocs, orderBy, query} from "firebase/firestore";
+import {db} from "../firebase/config";
+import {useEffect, useState} from "react";
 
 export const PostsScreen = () => {
+    const [posts, setPosts] = useState([])
+    async function fetchAllPosts() {
+        const data = []
+        const collectionRef = collection(db, "posts")
+        const q = query(collectionRef, orderBy("timestamp","desc"))
+        const querySnapshot= await getDocs(q)
+        querySnapshot.forEach((doc) => {
+            if(doc.data().uid===localStorage.getItem("uid")){
+                data.push(doc.data())
+            }
+        })
+        setPosts(data)
+    }
+
+
+    useEffect(() => {
+        try {
+            fetchAllPosts()
+        } catch (e) {
+            console.log(e.message)
+        }
+
+    }, [])
     return (
         <div className="lg:my-16">
-            <PostCard/>
-            <PostCard/>
-            <PostCard/>
+            {posts && posts.map((post)=>{
+                return <PostCard key={post.timestamp.seconds} post={post} />
+            })}
         </div>
     )
 }
