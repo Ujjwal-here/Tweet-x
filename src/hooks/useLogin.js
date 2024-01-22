28ac3fd0da3fc5a8c0ff106414f90ad51e522e11
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-23 00:14:18 +0530

    CHANGE & ADD: Change Identifier Name and Add Success, Error Messages

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,23 +1,24 @@
-import {useRef, useState} from "react";
+import {useContext, useRef} from "react";
 import {useNavigate} from "react-router-dom";
 import {signInWithEmailAndPassword} from "firebase/auth"
 import {auth} from "../firebase/config";
+import {TweetXContext} from "../context/TweetXContext";
 
 const useLogin = () => {
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState("")
-    const formRef = useRef()
+    const {isLoading, setIsLoading, success, setSuccess, error, setError} = useContext(TweetXContext)
+    const loginFormRef = useRef()
     const navigate = useNavigate()
 
-    async function loginHandleSubmit(e) {
+    async function handleLogin(e) {
         setIsLoading(true)
         e.preventDefault()
-        const email = formRef.current[0].value
-        const password = formRef.current[1].value
+        const email = loginFormRef.current[0].value
+        const password = loginFormRef.current[1].value
         try{
             const userCredential = await signInWithEmailAndPassword(auth,email,password)
             localStorage.setItem("token", userCredential.user.accessToken)
             localStorage.setItem("uid", userCredential.user.uid)
+            setSuccess("Logged In Successfully!")
             setIsLoading(false)
             navigate("/feed")
         }
@@ -26,6 +27,7 @@ const useLogin = () => {
             setIsLoading(false)
         }
     }
-    return {isLoading,setIsLoading,error,setError,formRef,loginHandleSubmit}
+
+    return {isLoading, setIsLoading, error, setError, loginFormRef, handleLogin}
 }
 export default useLogin
