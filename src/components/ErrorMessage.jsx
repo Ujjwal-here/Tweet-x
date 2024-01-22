a4e6221284c176d3f899387a477a676275868ba0
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-22 23:40:57 +0530

    REPLACE: Replace Dummy Message with Custom Message

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,4 +1,4 @@
-export const ErrorMessage = () => {
+export const ErrorMessage = ({message}) => {
     return (
         <div
             className="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
@@ -10,7 +10,7 @@ export const ErrorMessage = () => {
             </svg>
             <span className="sr-only">Info</span>
             <div>
-                <span className="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+                <span className="font-medium">Danger alert!</span>{message}
             </div>
         </div>
     )
