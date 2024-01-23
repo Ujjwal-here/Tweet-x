926671dccc80d5f0e376888246bec159ed42b1df
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-23 23:18:42 +0530

    ADD: Add Responsiveness upto Medium Sized Devices

--- src/screens/FeedScreen.jsx
+++ src/screens/FeedScreen.jsx
@@ -5,10 +5,11 @@ import {collection, getDocs, orderBy, query} from "firebase/firestore";
 import {db} from "../firebase/config";
 import {PostCard} from "../components/PostCard";
 import {ClipLoader} from "react-spinners";
+import {ErrorMessage} from "../components/ErrorMessage";
 
 export const FeedScreen = () => {
     const [posts, setPosts] = useState([])
-    const {isOpen, setIsOpen, isLoading, setIsLoading, success, setSuccess, error, setError} = useContext(TweetXContext)
+    const {setIsOpen, isLoading, setIsLoading, error, setError} = useContext(TweetXContext)
 
 
     function openModalHandler(){
@@ -20,21 +21,21 @@ export const FeedScreen = () => {
             setIsLoading(true)
             const data = []
             const collectionRef = collection(db, "posts")
-            const q = query(collectionRef, orderBy("timestamp","desc"))
-            const querySnapshot= await getDocs(q)
+            const querySnapshot= await getDocs(collectionRef)
             querySnapshot.forEach((doc) => {
-                data.push(doc.data())
+                const sortByTimestamp= doc.data().posts.sort(function (a,b){
+                    return b.timestamp.seconds- a.timestamp.seconds
+                })
+                data.push(...sortByTimestamp)
             })
             setPosts(data)
             setIsLoading(false)
-            setSuccess("Post Fetched Successfully!")
         }catch (e){
-            setError(e.message)
+            setError("Oops! Something went wrong. We couldn't fetch the posts at the moment. Please try again later")
             setIsLoading(false)
         }
 
     }
-
 
     useEffect(() => {
         fetchAllPosts()
@@ -42,7 +43,6 @@ export const FeedScreen = () => {
         return ()=>{
             setIsLoading(false)
             setError("")
-            setSuccess("")
         }
     }, [])
 
@@ -51,9 +51,10 @@ export const FeedScreen = () => {
     }
 
     return (
-        <div className="md:mx-24 lg:mx-48 xl:mx-72">
+        <div className="md:mx-24 lg:mx-48 xl:mx-72 2xl:mx-96">
+            {error && <ErrorMessage message={error}/> }
             <button onClick={openModalHandler} data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
-                    className="bg-[#FF748D] shadow-xl text-white rounded font-normal md:text-xs md:px-6 md:py-2 md:mt-10 lg:text-sm md:mx-16 lg:mx-20 xl:text-sm xl:px-6 xl:py-2  xl:mx-24 xl:mt-10">Create
+                    className="bg-[#FF748D] shadow-xl text-white rounded font-normal md:text-xs md:px-6 md:py-2 md:mt-10 lg:text-sm md:mx-16 lg:mx-20 xl:text-sm xl:px-6 xl:py-2 xl:mx-24 xl:mt-10 2xl:text-base">Create
                 Post
             </button>
             {posts && posts.map((post)=>{
