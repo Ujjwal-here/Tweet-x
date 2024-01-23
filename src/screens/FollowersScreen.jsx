1c4b9d5e81209dba907096f91765487c7fd990de
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-24 00:07:35 +0530

    ADD: Add Responsiveness upto Medium Sized Device

--- src/screens/FollowersScreen.jsx
+++ src/screens/FollowersScreen.jsx
@@ -1,11 +1,69 @@
 import {UserCard} from "../components/UserCard";
+import {useContext, useEffect, useState} from "react";
+import {TweetXContext} from "../context/TweetXContext";
+import {collection, documentId, getDocs, query, where} from "firebase/firestore";
+import {db} from "../firebase/config";
+import {ClipLoader} from "react-spinners";
+import {ErrorMessage} from "../components/ErrorMessage";
+import {InfoMessage} from "../components/InfoMessage";
 
 export const FollowersScreen = () => {
+    const {isLoading, setIsLoading, success, setSuccess, error, setError} = useContext(TweetXContext)
+
+    const [followerDetail, setFollowerDetail] = useState([])
+
+    const loggedInUid = localStorage.getItem("uid")
+
+    async function getUserFollowingsDetail() {
+        try {
+            setIsLoading(true)
+            let userFollowers = []
+            const usersRef = collection(db, 'users')
+            const queryOne = query(usersRef, where(documentId(), '==', loggedInUid))
+            const docSnapOne = await getDocs(queryOne)
+            docSnapOne.forEach((doc) => {
+                userFollowers = doc.data().followers
+            })
+
+            if (userFollowers.length !== 0) {
+                const userDetails = []
+                const queryTwo = query(usersRef, where(documentId(), 'in', userFollowers))
+                const docSnapTwo = await getDocs(queryTwo)
+                docSnapTwo.forEach((doc) => {
+                    userDetails.push(doc.data())
+                })
+                setFollowerDetail(userDetails)
+            }
+            setIsLoading(false)
+        } catch (e) {
+            setError(e.message)
+            setIsLoading(false)
+        }
+
+    }
+
+    useEffect(() => {
+        getUserFollowingsDetail()
+        return () => {
+            setIsLoading(true)
+            setError("")
+            setSuccess("")
+        }
+    }, []);
+
+    if (isLoading) {
+        return <div className="flex flex-row justify-center items-center my-20"><ClipLoader size={50} color="black"/>
+        </div>
+    }
+
     return (
-        <div className="lg:my-20">
-            <UserCard/>
-            <UserCard/>
-            <UserCard/>
+        <div>
+            {error && <div className="flex flex-row justify-center"><ErrorMessage message={error}/></div>}
+            {followerDetail.length === 0 &&
+                <div className="flex flex-row justify-center"><InfoMessage message="No Followers Found"/></div>}
+            {followerDetail && followerDetail.map((data) => {
+                return <UserCard user={data} profileStyle={true}/>
+            })}
         </div>
     )
 }
--- src/screens/FollowingScreen.jsx
+++ src/screens/FollowingScreen.jsx
@@ -1,11 +1,68 @@
+import {useContext, useEffect, useState} from "react";
+import {collection, documentId, getDocs, query, where} from "firebase/firestore";
+import {db} from "../firebase/config";
 import {UserCard} from "../components/UserCard";
+import {TweetXContext} from "../context/TweetXContext";
+import {ClipLoader} from "react-spinners";
+import {ErrorMessage} from "../components/ErrorMessage";
+import {InfoMessage} from "../components/InfoMessage";
 
 export const FollowingScreen = () => {
+    const {isLoading, setIsLoading, success, setSuccess, error, setError} = useContext(TweetXContext)
+
+    const [followingsDetail, setFollowingsDetail] = useState([])
+
+    const loggedInUid = localStorage.getItem("uid")
+
+    async function getUserFollowingsDetail() {
+        try {
+            setIsLoading(true)
+            let userFollowings = []
+            const usersRef = collection(db, 'users')
+            const queryOne = query(usersRef, where(documentId(), '==', loggedInUid))
+            const docSnapOne = await getDocs(queryOne)
+            docSnapOne.forEach((doc) => {
+                userFollowings = doc.data().following
+            })
+
+            if (userFollowings.length !== 0) {
+                const userDetails = []
+                const queryTwo = query(usersRef, where(documentId(), 'in', userFollowings))
+                const docSnapTwo = await getDocs(queryTwo)
+                docSnapTwo.forEach((doc) => {
+                    userDetails.push(doc.data())
+                })
+                setFollowingsDetail(userDetails)
+            }
+            setIsLoading(false)
+        } catch (e) {
+            setError(e.message)
+            setIsLoading(false)
+        }
+
+    }
+
+    useEffect(() => {
+        getUserFollowingsDetail()
+        return () => {
+            setError("")
+            setIsLoading(false)
+            setSuccess("")
+        }
+    }, []);
+
+    if (isLoading) {
+        return <div className="flex flex-row justify-center items-center my-20"><ClipLoader size={50} color="black"/>
+        </div>
+    }
     return (
-        <div className="lg:my-20">
-            <UserCard/>
-            <UserCard/>
-            <UserCard/>
+        <div className="sm:my-10">
+            {error && <div className="flex flex-row justify-center"><ErrorMessage message={error}/></div>}
+            {followingsDetail.length === 0 &&
+                <div className="flex flex-row justify-center"><InfoMessage message="No Followings Found"/></div>}
+            {followingsDetail && followingsDetail.map((data) => {
+                return <UserCard user={data}/>
+            })}
         </div>
     )
 }
