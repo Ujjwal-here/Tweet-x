d2c52eff7e18c1bc99799b9b0272f3e30c26bd91
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-23 13:04:26 +0530

    ADD: Add Responsiveness upto Medium Sized Devices

--- src/screens/FeedScreen.jsx
+++ src/screens/FeedScreen.jsx
@@ -1,43 +1,60 @@
 import {CreatePostModal} from "../components/CreatePostModal";
 import {useContext, useEffect, useState} from "react";
 import {TweetXContext} from "../context/TweetXContext";
-import {collection, getDocs, orderBy,query} from "firebase/firestore";
+import {collection, getDocs, orderBy, query} from "firebase/firestore";
 import {db} from "../firebase/config";
 import {PostCard} from "../components/PostCard";
+import {ClipLoader} from "react-spinners";
 
 export const FeedScreen = () => {
     const [posts, setPosts] = useState([])
-    const {isOpen,setIsOpen} = useContext(TweetXContext)
+    const {isOpen, setIsOpen, isLoading, setIsLoading, success, setSuccess, error, setError} = useContext(TweetXContext)
+
 
     function openModalHandler(){
-        setIsOpen(prevState=>!prevState)
+        setIsOpen(true)
     }
 
     async function fetchAllPosts() {
-        const data = []
-        const collectionRef = collection(db, "posts")
-        const q = query(collectionRef, orderBy("timestamp","desc"))
-        const querySnapshot= await getDocs(q)
-        querySnapshot.forEach((doc) => {
-           data.push(doc.data())
-        })
-        setPosts(data)
+        try {
+            setIsLoading(true)
+            const data = []
+            const collectionRef = collection(db, "posts")
+            const q = query(collectionRef, orderBy("timestamp","desc"))
+            const querySnapshot= await getDocs(q)
+            querySnapshot.forEach((doc) => {
+                data.push(doc.data())
+            })
+            setPosts(data)
+            setIsLoading(false)
+            setSuccess("Post Fetched Successfully!")
+        }catch (e){
+            setError(e.message)
+            setIsLoading(false)
+        }
+
     }
 
 
     useEffect(() => {
-        try {
-            fetchAllPosts()
-        } catch (e) {
-            console.log(e.message)
-        }
+        fetchAllPosts()
 
+        return ()=>{
+            setIsLoading(false)
+            setError("")
+            setSuccess("")
+        }
     }, [])
 
+    if (isLoading){
+        return <div className="flex flex-row justify-center items-center h-lvh"><ClipLoader size={50} color="black"/></div>
+    }
+
     return (
-        <div className="lg:mx-96">
+        <div className="md:mx-28 lg:mx-40 xl:mx-72">
             <button onClick={openModalHandler} data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
-                className="bg-[#FF748D] shadow-xl text-white rounded font-normal lg:text-sm lg:px-6 lg:py-2 lg:mx-24 lg:mt-10">Write
+                    className="bg-[#FF748D] shadow-xl text-white rounded font-normal md:text-xs md:px-6 md:py-2 md:mt-10 lg:text-sm lg:mx-24 xl:text-sm xl:px-6 xl:py-2 xl:mx-24 xl:mt-10">Create
+                Post
             </button>
             {posts && posts.map((post)=>{
                 return <PostCard key={post.timestamp.seconds} post={post} />
