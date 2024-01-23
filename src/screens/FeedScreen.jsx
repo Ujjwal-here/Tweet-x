acccab605ae8af7ed60662f3ca917289ff6d0bd4
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-01-23 23:25:14 +0530

    ADD: Add info message if posts are empty

--- src/screens/FeedScreen.jsx
+++ src/screens/FeedScreen.jsx
@@ -1,41 +1,16 @@
 import {CreatePostModal} from "../components/CreatePostModal";
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect} from "react";
 import {TweetXContext} from "../context/TweetXContext";
-import {collection, getDocs, orderBy, query} from "firebase/firestore";
-import {db} from "../firebase/config";
 import {PostCard} from "../components/PostCard";
 import {ClipLoader} from "react-spinners";
 import {ErrorMessage} from "../components/ErrorMessage";
+import {useFeed} from "../hooks/useFeed";
+import {InfoMessage} from "../components/InfoMessage";
 
 export const FeedScreen = () => {
-    const [posts, setPosts] = useState([])
-    const {setIsOpen, isLoading, setIsLoading, error, setError} = useContext(TweetXContext)
+    const {isLoading, setIsLoading, error, setError} = useContext(TweetXContext)
 
-
-    function openModalHandler(){
-        setIsOpen(true)
-    }
-
-    async function fetchAllPosts() {
-        try {
-            setIsLoading(true)
-            const data = []
-            const collectionRef = collection(db, "posts")
-            const querySnapshot= await getDocs(collectionRef)
-            querySnapshot.forEach((doc) => {
-                const sortByTimestamp= doc.data().posts.sort(function (a,b){
-                    return b.timestamp.seconds- a.timestamp.seconds
-                })
-                data.push(...sortByTimestamp)
-            })
-            setPosts(data)
-            setIsLoading(false)
-        }catch (e){
-            setError("Oops! Something went wrong. We couldn't fetch the posts at the moment. Please try again later")
-            setIsLoading(false)
-        }
-
-    }
+    const {fetchAllPosts, posts, openModalHandler} = useFeed()
 
     useEffect(() => {
         fetchAllPosts()
@@ -52,6 +27,7 @@ export const FeedScreen = () => {
 
     return (
         <div className="md:mx-24 lg:mx-48 xl:mx-72 2xl:mx-96">
+            {posts.length === 0 && <InfoMessage message="No Post Found !"/>}
             {error && <ErrorMessage message={error}/> }
             <button onClick={openModalHandler} data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
                     className="bg-[#FF748D] shadow-xl text-white rounded font-normal md:text-xs md:px-6 md:py-2 md:mt-10 lg:text-sm md:mx-16 lg:mx-20 xl:text-sm xl:px-6 xl:py-2 xl:mx-24 xl:mt-10 2xl:text-base">Create
